1915d66dd7b4c67c6d0077704c6969a2d5173f9f
_sam <3804518+aahung@users.noreply.github.com>
2021-06-28 15:24:57 -0700

    Show gif results!

--- App.tsx
+++ App.tsx
@@ -1,24 +1,61 @@
 import {useRequest} from 'ahooks';
-import React, {useEffect, useMemo, useState} from 'react';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   TextInput,
   Text,
+  Image,
+  FlatList,
+  ListRenderItem,
 } from 'react-native';
 import * as querystring from 'query-string';
 import debounce from 'debounce';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
+interface GifImage {
+  size: string;
+  url: string;
+}
+
+interface Gif {
+  title: string;
+  id: string;
+  images: {
+    original: GifImage;
+    downsized: GifImage;
+    fixed_height_small: GifImage;
+  };
+}
+
+interface SearchResult {
+  data: Gif[];
+}
+
 const GIPHY_KEY = 'lBkWp4xWgwegUbSnnms1bsvPMX0zoT2i'; // some dummy account, ok to include in git
+
+const GifView: React.FC<{gif: Gif}> = ({gif}) => {
+  return (
+    <View>
+      <Text style={[styles.text, styles.imageText]} numberOfLines={1}>
+        {gif.title || gif.id}
+      </Text>
+      <Image
+        style={styles.image}
+        height={200}
+        width={267}
+        source={{uri: gif.images.fixed_height_small.url}}
+      />
+    </View>
+  );
+};
 
 const App: React.FC = () => {
   const [searchText, setSearchText] = useState('');
 
-  const {data, run, loading} = useRequest(
+  const {data, run, loading} = useRequest<SearchResult>(
     (query: string) => ({
       url: querystring.stringifyUrl({
         url: 'https://api.giphy.com/v1/gifs/search',
@@ -36,39 +73,38 @@ const App: React.FC = () => {
     }
   }, [debouncedSearch, searchText]);
 
+  const renderItem: ListRenderItem<Gif> = useCallback(
+    ({item}) => <GifView gif={item} />,
+    [],
+  );
+
   return (
     <>
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <TextInput
-            style={styles.input}
-            onChangeText={setSearchText}
-            value={searchText}
-          />
-          <View style={styles.body}>
-            {loading && <Text style={styles.text}>Loading</Text>}
+      <SafeAreaView style={styles.body}>
+        <TextInput
+          style={styles.input}
+          onChangeText={setSearchText}
+          value={searchText}
+        />
+        {loading && <Text style={styles.text}>Loading</Text>}
 
-            <Text style={styles.text}>
-              {searchText && data
-                ? JSON.stringify(data)
-                : 'Please input some query'}
-            </Text>
-          </View>
-        </ScrollView>
+        <FlatList<Gif>
+          style={styles.list}
+          data={data?.data || []}
+          renderItem={renderItem}
+          keyExtractor={(item: Gif) => item.id}
+          numColumns={3}
+        />
       </SafeAreaView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
   body: {
     backgroundColor: Colors.white,
     color: Colors.black,
+    flex: 1,
   },
   input: {
     height: 40,
@@ -78,6 +114,16 @@ const styles = StyleSheet.create({
   },
   text: {
     color: Colors.black,
+  },
+  imageText: {
+    width: 266,
+  },
+  image: {
+    width: 266,
+    height: 200,
+  },
+  list: {
+    marginLeft: 15,
   },
 });
 
